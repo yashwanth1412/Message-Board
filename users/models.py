283ff338c5e3c0e9d41782416f6df2e37296ee06
yashwanth1412 <yashvallabhu@gmail.com>
2021-06-19 23:40:27 +0530

    Added signals to create profile for the user

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
     profile_pic = models.ImageField(upload_to="profile_pics/", default="avatar.png")
-    bio = models.TextField()
+    bio = models.TextField(default="No bio")
 
     def __str__(self):
         return f"{self.user.username}'s profile" 
