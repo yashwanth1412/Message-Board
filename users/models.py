235b4332aa2757431255b546b050c40e2eb9e205
yashwanth1412 <yashvallabhu@gmail.com>
2021-06-19 19:52:45 +0530

    Added nav bar

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name="profile")
+    profile_pic = models.ImageField(upload_to="profile_pics/", default="avatar.png")
+    bio = models.TextField()
